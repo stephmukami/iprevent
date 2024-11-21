f08602b6011070cd794a292077b1f7032523e6a3
stephmukami <stephmukami@gmail.com>
2024-11-21 21:04:45 +0300

    refactor: added predicted component into a suspense block

--- src/app/predicted/page.tsx
+++ src/app/predicted/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../(components)/Navbar";
 import Footer from "../(components)/Footer";
 import Prediction from "../(components)/Prediction";
+import { Suspense } from 'react'
 
 type Props = object;
 
@@ -9,7 +10,12 @@ function page({}: Props) {
   return (
     <>
       <Navbar />
-      <Prediction/>
+
+      <Suspense>
+        <Prediction/>
+      </Suspense>
+
+
       <Footer />
     </>
   );
